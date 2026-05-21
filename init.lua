711247e9ec7f44c65043714e60312c8633095d79
Santiago Rodriguez <santiago@athena.local>
2026-05-21 09:30:14 -0400

    Added dotfiles for the first time

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Core option changes.
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.sw = 2
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.background = 'dark'
+vim.g.mapleader = ' '
