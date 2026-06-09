b65409183edf927a016d79706ef36e83c8f926ff
Santiago Rodriguez <santo1996.29@gmail.com>
2026-06-09 18:41:35 -0400

    Update the nvim files

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,7 @@ vim.o.sw = 2
 vim.o.expandtab = true
 vim.o.tabstop = 2
 -- vim.cmd.color 'habamax'
-vim.o.background = 'dark'
+vim.o.background = 'light'
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
@@ -17,7 +17,6 @@ vim.g.maplocalleader = '\\'
 require('lazy_setup')
 require('harpoon_setup')
 require('lualine_setup')
-require('vague_setup')
 require('persistence_setup')
 require('gitsigns_setup')
 require('nvim_treesitter_setup')
