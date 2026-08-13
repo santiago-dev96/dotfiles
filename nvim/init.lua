4f14c836ce29db551c277fb85db5c15a7d9eaad8
Santiago Rodriguez <santo1996.29@gmail.com>
2026-08-13 14:42:03 -0400

    Start with the Neovim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+require('config.lazy')
+
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
@@ -6,5 +8,3 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.cmd.color 'habamax'
 vim.o.background = 'dark'
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
