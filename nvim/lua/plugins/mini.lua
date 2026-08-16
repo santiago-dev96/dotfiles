1c62990ee01cf3072261d91042b8fd9c4ace78dc
Santiago Rodriguez <santo1996.29@gmail.com>
2026-08-16 12:35:45 -0400

    install mini and gitsigns

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-mini/mini.nvim',
+  version = false,
+  config = function()
+    require('mini.ai').setup()
+    require('mini.operators').setup()
+    require('mini.surround').setup()
+    require('mini.comment').setup()
+  end
+}
