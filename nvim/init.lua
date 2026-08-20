5a2ad04a445c9ddc6675e6011d682d7ef38c932d
Santiago Rodriguez <santo1996.29@gmail.com>
2026-08-20 12:16:20 -0400

    Simplify dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,8 @@
-require('config.lazy')
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
 vim.o.sw = 2
 vim.o.expandtab = true
 vim.o.tabstop = 2
-vim.cmd.color 'tokyonight'
+vim.cmd.color 'habamax'
 vim.o.background = 'dark'
