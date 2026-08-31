4e3abfee099b47aeb7620183291c7aab2e55ae5d
Santiago Rodriguez <santo1996.29@gmail.com>
2026-08-31 14:02:49 -0400

    Simplified things again

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,8 @@
-require('config.lazy')
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
 vim.o.sw = 2
 vim.o.expandtab = true
 vim.o.tabstop = 2
-vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd[[colorscheme retrobox]]
 vim.o.background = 'dark'
-vim.o.showmode = false
