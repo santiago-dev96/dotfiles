dbcff339a93e96ecdf83f8e088e2a7abef55e554
Santiago Rodriguez <santo1996.29@gmail.com>
2026-09-17 10:25:08 -0400

    Change foldmethod per filetype

--- nvim/init.lua
+++ nvim/init.lua
@@ -164,7 +164,18 @@ vim.cmd.colorscheme 'catppuccin-mocha'
 vim.o.background = 'dark'
 
 -- Folding
-vim.o.foldmethod = 'syntax'
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' },
+  callback = function()
+    vim.o.foldmethod = 'syntax'
+  end
+})
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'lua',
+  callback = function()
+    vim.o.foldmethod = 'expr'
+  end
+})
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
